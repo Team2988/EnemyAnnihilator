ea89ef6207d97555478bb3e5c3387649569a7480
LeviBodnar <58431060+LeviBodnar@users.noreply.github.com>
2020-01-28 15:48:59 -0500

    changed
    niothuing

--- EnemyAnihilator/src/main/java/frc/robot/Main.java
+++ EnemyAnihilator/src/main/java/frc/robot/Main.java
@@ -15,6 +15,11 @@ import edu.wpi.first.wpilibj.RobotBase;
  * change the parameter class to the startRobot call.
  */
 public final class Main {
+  JoyStick driveStick = new Joystick(RobotMap.RIGHT_STICK)
+  public Joystick rightstick = new Joystick(RobotMap.RIGHT_STICK);
+  
+  public static Joystick rightStickGetter() {
+    return rightstick()
   private Main() {
   }
 
